4fa6cf69196ba1da2303913a92522530aaa1ea16
gengwenhao <gengwenhao97@126.com>
2018-12-05 10:05:02 +0800

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import db.DBHelper;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        ResultSet stuList = new DBHelper().getStudentList();
-        System.out.println(stuList);
+        boolean isLogin = new DBHelper().login("20185239221", "admin");
+        System.out.println(isLogin);
     }
 }
--- src/db/DBHelper.java
+++ src/db/DBHelper.java
@@ -1,7 +1,5 @@
 package db;
 
-import jdk.nashorn.internal.scripts.JD;
-
 import java.sql.*;
 
 class DBConnection {
@@ -53,13 +51,83 @@ class DBConnection {
 
         return null;
     }
+
+    // 批量查询
+    ResultSet getQueryMany(String sql1, String sql2) {
+        try {
+            if (null == stmt || null == conn) Connect();
+            stmt.addBatch(sql1);
+            return stmt.executeQuery(sql2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // 插入删除更新
+    int update(String sql) {
+        try {
+            if (null == stmt || null == conn) Connect();
+            return stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
 }
 
 public class DBHelper extends DBConnection {
+    // 查看学生列表
     public ResultSet getStudentList() {
-        ResultSet rs = null;
-        rs = getQuery("SELECT sid, sno, sname, sgender, sbirthday FROM student;");
+        return getQuery("SELECT sid, sno, sname, sgender, sbirthday FROM student;");
+    }
 
-        return rs;
+    // 登录
+    public boolean login(String username, String pwd) {
+        String sql1 = "SELECT username FROM superuser WHERE username='" + username + "' and password='" + pwd + "';";
+        String sql2 = "SELECT sno FROM student WHERE sno='" + username + "' and password='" + pwd + "';";
+
+        ResultSet rs = getQueryMany(sql1, sql2);
+        try {
+            if (null != rs)
+                return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    // 添加学生信息
+    public int addStuProfile(String sno, String name, String password,
+                             int gender, int classID) {
+        if (null == sno || null == name || null == password)
+            return -1;
+
+        String sql = "INSERT INTO student (sno, sname, password, sgender, class_id) values ('" + sno + "','" + name + "','" + password + "'," + gender + "," + classID + ");";
+        return update(sql);
+    }
+
+    // 通过SID查看学生成绩
+    public ResultSet selectStuScorebySID(int sid) {
+        String sql = "SELECT * FROM score WHERE student_id=" + sid + ";";
+
+        return getQuery(sql);
+    }
+
+    // 查看学生某一科目成绩
+    public ResultSet selectStuScorebySIDandCID(int sid, int cid) {
+        String sql = "SELECT * FROM score WHERE student_id=" + sid + " and course_id=" + cid + ";";
+
+        return getQuery(sql);
+    }
+
+    public boolean updateStuPWD(String newPWD, int sid) {
+        String sql = "UPDATE student SET password='" + newPWD + "' WHERE sid=" + sid + ";";
+
+        return update(sql) > 0;
     }
 }
